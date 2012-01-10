d1a0b7d8b117e49a7e1bbdde5278e58a7fd7c664
AWARA Zeppachi <awarazeppachi@gmail.com>
2012-01-10 16:26:03 +0900

    芸術技能の追加削除の仕込み

--- hkc/html/hkc.js
+++ hkc/html/hkc.js
@@ -1,3 +1,9 @@
+/*
+	放課後怪奇くらぶキャラクター作成支援ツール
+	あれこれ用JavaScript
+	書いた人：zeppachi
+*/
+//	部活動で有効化される技能欄。MagicNumber!?ナンデ?
 var club = {
   0 : [13, 22, 29, 7],
   1 : [18, 12, 29, 20, 14],
@@ -17,9 +23,15 @@ var club = {
  15 : [46,6,1],
  16 : [46,2,29,38,37],
  17 : [1,37,2,10,31,29],
- 18 : [46,7]
+ 18 : [46,7],
+ 19 : [101]
+};
+var extra_art = {
+ 101: '野球'
 };
 const WARNING_COLOR = "#eb6101";
+var   skill_num = 49;
+const DEFAULT_SKILL_NUM = 49;
 //  onReady go!
 $(document).ready(function(){clubSkillPointLock();});
 //  ダイスを振るよーー！
@@ -34,6 +46,7 @@ function parameter(){
   $('#POW').val(dice(3,6));
   $('#SIZ').val(dice(3,6));
   $('#EDU').val(dice(3,6)+3);
+  //自動計算
   onKeyUpInt();
   onKeyUpPow();
   onKeyUpEdu();
@@ -45,21 +58,27 @@ function parameter(){
 }
 //  副能力値の決定
 function setSubParameter(src, target){$(target).val($(src).val() * 5);};
+// [INT]を計算。整数型じゃないよ
 function onKeyUpInt(){setSubParameter('#INT', '#IDE');calcFreePoint();};
+// [POW]を計算。
 function onKeyUpPow(){setSubParameter('#POW', '#SAN');setSubParameter('#POW', '#LUC');calcMP();};
+// [EDU]を計算。
 function onKeyUpEdu(){setSubParameter('#EDU', '#KNW');calcSkillClub();};
+// [HP]を計算
 function calcHP(){
   var hp = Math.ceil( ($('#CON').val() * 1 +$('#SIZ').val() * 1)/2);
   $('#HP').val(hp);
 };
+// [MP]を計算。
 function calcMP(){$('#MP').val($('#POW').val())};
+// 部活動で割り振れるポイントの計算
 function calcSkillClub(){
   //  最大値
   var max_skillpoint = $('#EDU').val() * 20;
   //  現在利用している値
   var sum = 0;
-  for (id=1;id<=49;id++){
-sum = sum + $('#club'+id).val() * 1;
+  for (id=1;id<=skill_num;id++){
+    sum = sum + $('#club'+id).val() * 1;
   }
   $('#skill_club').html(max_skillpoint-sum);
   if (max_skillpoint-sum < 0) {
@@ -68,12 +87,13 @@ sum = sum + $('#club'+id).val() * 1;
     normalHeader();
   }
 }
+// 自由に割り振れるポイントの計算
 function calcFreePoint(){
   //  最大値
   var max_skillpoint = $('#INT').val() * 10;
   //  現在利用している値
   var sum = 0;
-  for (id=1;id<=49;id++){
+  for (id=1;id<=skill_num;id++){
     sum = sum + $('#prof'+id).val() * 1;
   }
   $('#skill_free').html(max_skillpoint-sum);
@@ -83,9 +103,10 @@ function calcFreePoint(){
     normalHeader();
   }
 }
+// 全ポイントの総計を取得
 function calcTotalSkillPoint()
 {
-  for (id=1;id<=49;id++){
+  for (id=1;id<=skill_num;id++){
     $('#total' +id).val(
       $('#prof'+id).val() * 1
     + $('#club'+id).val() * 1
@@ -93,24 +114,33 @@ function calcTotalSkillPoint()
     );
   }
 }
+// 部活動で割り振れるポイント入力欄をロックする
 function clubSkillPointLock()
 {
-  for (id=1;id<=49;id++) {
+  for (id=1;id<=skill_num;id++) {
     $('#club' + id).attr('readonly', true);
     $('#club' + id).css('background', '#999');
   }
 }
+// 選択できる技能を強調表示する
 function markUpSkill()
 {
   var club_id = $('#club option:selected').val();
-  if ('' == club_id) {
-return;
-  }
+  if ('' == club_id) {return;}
+  // スキル欄の初期化
+  removeExtraArtisticSkill();
   var club_data = club[club_id];
   clubSkillPointLock();
   for (idx in club_data) {
-    $('#club' + club_data[idx]).attr('readonly', false);
-    $('#club' + club_data[idx]).css('background', 'white');
+    // 追加芸術?
+    var skill_id = club_data[idx];
+    if (100 < club_data[idx]) {
+	addExtraArtisticSkill(extra_art[club_data[idx]]);
+	skill_id = skill_num;
+    };
+    $('#club' + skill_id).attr('readonly', false);
+    $('#club' + skill_id).css('background', 'white');
+   
   }
 }
 //  部活動ポイントが変わった
@@ -119,6 +149,7 @@ function clubKeyUp()
   calcSkillClub();
   calcTotalSkillPoint();
 }
+//  自由入力ポイント欄が変わった
 function profKeyUp()
 {
   calcFreePoint();
@@ -130,9 +161,30 @@ function warningHeader()
     $('header').css('background-color', WARNING_COLOR);
     $('header').css('box-shadow-color', WARNING_COLOR);
 }
+//　もとに戻すよー！
 function normalHeader()
 {
     $('header').css('background','black');
     $('header').css('box-shadow-color','black');
 }
+//  [芸術]技能の入力欄を追加する
+function addExtraArtisticSkill(name)
+{
+  skill_num++;
+  $('#skill_sheet_3').append(getArtisticSkillRow('芸術/' + name, '05', skill_num));
+}
+// 削除
+function removeExtraArtisticSkill()
+{
+  for (i=DEFAULT_SKILL_NUM + 1;i<=skill_num;i++){
+   $('#row' +i).remove();
+  }
+  skill_num = DEFAULT_SKILL_NUM * 1;
+}
+//
+function getArtisticSkillRow(name, default_point, id)
+{
+  return "<tr id='row" + id + "'><td>" + name + "</td><td id='pre" + id + "'>" + default_point + "</td><td><input type='text' size='3' maxlength='3' onKeyUp='clubKeyUp()' id='club" + id + "'></td><td><input type='text' size='3' maxlength='3' onKeyUp='profKeyUp()' id='prof" + id + "'></td><td><input type='text' size='3' maxlength='3' readonly='readonly' id='total" + id + "'></td></tr>";
+}
+
 
