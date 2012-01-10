c09a3ad269674ebb78a5a8fcfa7b06ac77214c85
AWARA Zeppachi <awarazeppachi@gmail.com>
2012-01-10 18:00:16 +0900

    JavaScriptを整理

--- hkc/html/hkc.js
+++ hkc/html/hkc.js
@@ -3,39 +3,7 @@
 	あれこれ用JavaScript
 	書いた人：zeppachi
 */
-//	部活動で有効化される技能欄。MagicNumber!?ナンデ?
-var club = {
-  0 : [13, 22, 29, 7],
-  1 : [18, 12, 29, 20, 14],
-  2 : [15, 18, 29, 17],
-  3 : [24,5,42,46],
-  4 : [27,24,46],
-  5 : [26,25,24,46],
-  6 : [48, 24, 46],
-  7 : [48, 42, 24, 40, 46],
-  8 : [42, 40, 24, 46],
-  9 : [25,28,42,46],
- 10 : [48,42,24,46],
- 11 : [39, 22, 42, 46],
- 12 : [1, 2, 36, 31],
- 13 : [1,46, 10,7,5],
- 14 : [46,6,32],
- 15 : [46,6,1],
- 16 : [46,2,29,38,37],
- 17 : [1,37,2,10,31,29],
- 18 : [46,7],
- 19 : [101]
-};
-var extra_art = {
- 101: '野球'
-};
-const WARNING_COLOR = "#eb6101";
-var   skill_num = 49;
-const DEFAULT_SKILL_NUM = 49;
-//  onReady go!
-$(document).ready(function(){clubSkillPointLock();});
-//  ダイスを振るよーー！
-function dice(n, m){return 1 == n ? Math.floor(Math.random()*m)+1 : dice(1, m) + dice(n-1, m);};
+
 //  能力値をダイス振って決めたいあなたへ
 function parameter(){
   $('#STR').val(dice(3,6));
@@ -56,21 +24,6 @@ function parameter(){
   calcFreePoint();
   calcTotalSkillPoint();
 }
-//  副能力値の決定
-function setSubParameter(src, target){$(target).val($(src).val() * 5);};
-// [INT]を計算。整数型じゃないよ
-function onKeyUpInt(){setSubParameter('#INT', '#IDE');calcFreePoint();};
-// [POW]を計算。
-function onKeyUpPow(){setSubParameter('#POW', '#SAN');setSubParameter('#POW', '#LUC');calcMP();};
-// [EDU]を計算。
-function onKeyUpEdu(){setSubParameter('#EDU', '#KNW');calcSkillClub();};
-// [HP]を計算
-function calcHP(){
-  var hp = Math.ceil( ($('#CON').val() * 1 +$('#SIZ').val() * 1)/2);
-  $('#HP').val(hp);
-};
-// [MP]を計算。
-function calcMP(){$('#MP').val($('#POW').val())};
 // 部活動で割り振れるポイントの計算
 function calcSkillClub(){
   //  最大値
@@ -142,30 +95,6 @@ function markUpSkill()
     $('#club' + skill_id).css('background', 'white');
    
   }
-}
-//  部活動ポイントが変わった
-function clubKeyUp()
-{
-  calcSkillClub();
-  calcTotalSkillPoint();
-}
-//  自由入力ポイント欄が変わった
-function profKeyUp()
-{
-  calcFreePoint();
-  calcTotalSkillPoint();
-}
-//  ヘッダ要素の見た目で遊ぶ
-function warningHeader()
-{
-    $('header').css('background-color', WARNING_COLOR);
-    $('header').css('box-shadow-color', WARNING_COLOR);
-}
-//　もとに戻すよー！
-function normalHeader()
-{
-    $('header').css('background','black');
-    $('header').css('box-shadow-color','black');
 }
 //  [芸術]技能の入力欄を追加する
 function addExtraArtisticSkill(name)
